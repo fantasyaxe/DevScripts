55c019f3b311c341dd2035bc304801d5521da04b
Nionim (C!tory¿) <delta.cion.fe@gmail.com>
2026-01-21 20:54:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # Edit this line if u need another mirror
-# Supported formats: http://, https://, file://
+# Supported formats: http://, https://, file:///
 
 # Ex:
 # MIRROR_URL = "https://raw.githubusercontent.com/MCRebooted/MirrorsList/refs/heads/main/mirrors.json"
 # MIRROR_URL = "file:///C:/DevScripts/mirrors.json"
+# MIRROR_URL = "file:///home/DevScripts/mirrors.json"
 
 
 MIRROR_URL = "https://raw.githubusercontent.com/MCRebooted/MirrorsList/refs/heads/main/mirrors.json"
